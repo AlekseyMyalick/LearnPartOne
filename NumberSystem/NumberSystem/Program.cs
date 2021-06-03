366e571ab20126ef371d7237537bf829c3212a22
Alexey Myalik <myalick2017@yandex.ru>
2021-06-03 12:21:42 +0300

    Add Convertor method.

---
+++ NumberSystem/NumberSystem/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace NumberSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string result = Converter(int.Parse(args[0]), int.Parse(args[1]));
+                Console.WriteLine(result);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        static string Converter(int number, int numberSystem)
+        {
+            if (number < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(number), "Can not be less than zero.");
+            }
+
+            if (numberSystem > 20 || numberSystem < 2)
+            {
+                throw new ArgumentOutOfRangeException(nameof(numberSystem), "Must be greater than 2 and less than 20");
+            }
+
+            char remainder;
+            string answer = string.Empty;
+            char[] alphabet = { 'A', 'B', 'C', 'D', 'E', 'F', 'G'};
+
+            while (number != 0)
+            {
+                remainder = number % numberSystem > 9 ? alphabet[(number % numberSystem) % 10] : (char)((number % numberSystem) + '0');
+                number /= numberSystem;
+                answer = remainder.ToString() + answer;
+            }
+
+            return answer;
+        }
+    }
+}
