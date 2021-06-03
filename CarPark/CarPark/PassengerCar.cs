b80345d152459adcc7c095cac9b563f81f1063d1
Alexey Myalik <myalick2017@yandex.ru>
2021-06-03 22:25:48 +0300

    Add PessengerCar class.

---
+++ CarPark/CarPark/PassengerCar.cs
@@ -0,0 +1,18 @@
+﻿namespace CarPark
+{
+    class PassengerCar : Vehicle
+    {
+        public int DoorsCount { get; set; }
+
+        public PassengerCar (Engine engine, Chassis chassis, Transmission transmission, int doorsCount) 
+            : base(engine, chassis, transmission)
+        {
+            DoorsCount = doorsCount;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + $"Doors count: {DoorsCount}\n";
+        }
+    }
+}
