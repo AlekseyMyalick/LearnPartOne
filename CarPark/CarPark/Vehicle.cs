739a27cfc1827002b1ea69a9e7e1ff3c8708033f
Alexey Myalik <myalick2017@yandex.ru>
2021-06-03 22:16:11 +0300

    Add abstract class Vehicle.

---
+++ CarPark/CarPark/Vehicle.cs
@@ -0,0 +1,23 @@
+﻿namespace CarPark
+{
+    abstract class Vehicle
+    {
+        public Engine VehicleEngine { get; set; }
+
+        public Chassis VehicleChassis { get; set; }
+
+        public Transmission VehicleTransmission { get; set; }
+
+        public Vehicle (Engine engine, Chassis chassis, Transmission transmission)
+        {
+            VehicleEngine = engine;
+            VehicleChassis = chassis;
+            VehicleTransmission = transmission;
+        }
+
+        public override string ToString()
+        {
+            return $"Engine:\n {VehicleEngine}\n Chassis:\n {VehicleChassis}\n Transmission:\n {VehicleTransmission}\n";
+        }
+    }
+}
