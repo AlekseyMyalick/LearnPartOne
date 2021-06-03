5e1eb940b0b1c28b162d5afc665169801100dabf
Alexey Myalik <myalick2017@yandex.ru>
2021-06-03 14:21:59 +0300

    Add Chassis class.

---
+++ CarPark/CarPark/Chassis.cs
@@ -0,0 +1,23 @@
+﻿namespace CarPark
+{
+    public class Chassis
+    {
+        public int WheelsCount { get; set; }
+
+        public string SerialNumber { get; set; }
+
+        public double PermissibleLoad { get; set; }
+
+        public Chassis (int wheelsCount, string serialNumber, double permissibleLoad)
+        {
+            WheelsCount = wheelsCount;
+            SerialNumber = serialNumber;
+            PermissibleLoad = permissibleLoad;
+        }
+
+        public override string ToString()
+        {
+            return $"Wheels count: {WheelsCount} \nSerial number: {SerialNumber} \nPermissible load: {PermissibleLoad} tons \n";
+        }
+    }
+}
