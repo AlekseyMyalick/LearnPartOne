15a7b0be90b3c08f2162ed37177878c4268c199d
Alexey Myalik <myalick2017@yandex.ru>
2021-06-05 23:33:20 +0300

    Add VehicleModel class.

---
+++ CarPark/CarPark/Models/VehicleModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CarPark.Models
+{
+    [Serializable]
+    public class VehicleModel
+    {
+        public EngineType EngineType { get; set; }
+
+        public string SerialNumber { get; set; }
+
+        public double Power { get; set; }
+
+        public VehicleModel() { }
+
+        public VehicleModel (EngineType engineType, string serialNumber, double power)
+        {
+            EngineType = engineType;
+            SerialNumber = serialNumber;
+            Power = power;
+        }
+    }
+}
