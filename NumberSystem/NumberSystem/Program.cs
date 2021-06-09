aa0be880bf743c5682e682a4ef833d0afc8d15b8
Alexey Myalik <myalick2017@yandex.ru>
2021-06-09 12:55:35 +0300

    Add some changes.

--- NumberSystem/NumberSystem/Program.cs
+++ NumberSystem/NumberSystem/Program.cs
@@ -4,6 +4,8 @@ namespace NumberSystem
 {
     class Program
     {
+        static readonly char[] alphabet = { 'A', 'B', 'C', 'D', 'E', 'F', 'G' };
+
         static void Main(string[] args)
         {
             try
@@ -19,28 +21,45 @@ namespace NumberSystem
 
         static string Converter(int number, int numberSystem)
         {
-            if (number < 0)
-            {
-                throw new ArgumentOutOfRangeException(nameof(number), "Can not be less than zero.");
-            }
-
             if (numberSystem > 20 || numberSystem < 2)
             {
                 throw new ArgumentOutOfRangeException(nameof(numberSystem), "Must be greater than 2 and less than 20");
             }
 
             char remainder;
+            string numberSign = GetNumberSign(number);
             string answer = string.Empty;
-            char[] alphabet = { 'A', 'B', 'C', 'D', 'E', 'F', 'G'};
+
+            ChangeNumberSign(ref number);
 
             while (number != 0)
             {
-                remainder = number % numberSystem > 9 ? alphabet[(number % numberSystem) % 10] : (char)((number % numberSystem) + '0');
+                remainder = GetRemainderSymbol(number % numberSystem);
                 number /= numberSystem;
                 answer = remainder.ToString() + answer;
             }
 
-            return answer;
+            return numberSign + answer;
+        }
+
+        static string GetNumberSign(int number)
+        {
+            return number < 0 ? "-" : string.Empty;
+        }
+
+        static void ChangeNumberSign(ref int number)
+        {
+            number = number < 0 ? -number : number;
+        }
+
+        static char GetRemainderSymbol(int number)
+        {
+            return number > 9 ? alphabet[number % 10] : ToChar(number);
+        } 
+
+        static char ToChar(int number)
+        {
+            return (char)(number + '0');
         }
     }
 }
