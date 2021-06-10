d22edcdbea74d7a4dcb7307ff1b6f0a753d9fdc2
Alexey Myalik <myalick2017@yandex.ru>
2021-06-10 22:16:35 +0300

    Add some changes to the Coordinate class.

--- InterfacesAndAbstractClasses/InterfacesAndAbstractClasses/Base/Coordinate.cs
+++ InterfacesAndAbstractClasses/InterfacesAndAbstractClasses/Base/Coordinate.cs
@@ -7,7 +7,13 @@ namespace InterfacesAndAbstractClasses
         private double _x;
         private double _y;
         private double _z;
-        
+
+        /// <summary>
+        /// Creates a point with a coordinate (X, Y, Z).
+        /// </summary>
+        /// <param name="x">X coordinate.</param>
+        /// <param name="y">Y coordinate.</param>
+        /// <param name="z">Z coordinate.</param>
         public Coordinate (double x, double y, double z)
         {
             _x = x;
@@ -15,6 +21,11 @@ namespace InterfacesAndAbstractClasses
             _z = z;
         }
 
+        /// <summary>
+        /// Returns the distance between two points.
+        /// </summary>
+        /// <param name="coordinate">The coordinate of the end point.></param>
+        /// <returns>Distance between two points</returns>
         public double Distance(Coordinate coordinate)
         {
             return Math.Sqrt(Math.Pow((coordinate._x - _x), 2) + 
