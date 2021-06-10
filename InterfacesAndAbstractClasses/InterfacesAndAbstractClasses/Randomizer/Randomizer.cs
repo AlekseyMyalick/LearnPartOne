999232d0cb5512d661b18662f14ab5e19758fc2b
Alexey Myalik <myalick2017@yandex.ru>
2021-06-10 22:37:29 +0300

    Add some changes to the Randomizer class.

--- InterfacesAndAbstractClasses/InterfacesAndAbstractClasses/Randomizer/Randomizer.cs
+++ InterfacesAndAbstractClasses/InterfacesAndAbstractClasses/Randomizer/Randomizer.cs
@@ -6,6 +6,12 @@ namespace InterfacesAndAbstractClasses
     {
         private static readonly Random random = new Random((int)DateTime.Now.Ticks);
 
+        /// <summary>
+        /// Generates a random number from start to end.
+        /// </summary>
+        /// <param name="start">The minimum value of the range.</param>
+        /// <param name="end">The maximum value of the range.</param>
+        /// <returns>Random number from start to end.</returns>
         public static int GeneratesRandomSpeed(int start, int end)
         {
             return random.Next(start, end);
