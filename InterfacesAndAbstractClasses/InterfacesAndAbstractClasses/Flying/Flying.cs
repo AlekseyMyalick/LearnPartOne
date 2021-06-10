446295ca40e6a7af5f5d127b9c03e938dea58977
Alexey Myalik <myalick2017@yandex.ru>
2021-06-10 22:35:02 +0300

    Add some changes to the Flying class.

--- InterfacesAndAbstractClasses/InterfacesAndAbstractClasses/Flying/Flying.cs
+++ InterfacesAndAbstractClasses/InterfacesAndAbstractClasses/Flying/Flying.cs
@@ -4,6 +4,10 @@
     {
         public Coordinate CurrentPosition { get; set; }
 
+        /// <summary>
+        /// Creates an object at the current coordinate.
+        /// </summary>
+        /// <param name="currentPosition">Current position.</param>
         public Flying (Coordinate currentPosition)
         {
             CurrentPosition = currentPosition;
