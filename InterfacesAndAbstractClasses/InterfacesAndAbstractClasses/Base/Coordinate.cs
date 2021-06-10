2796be853a52cc23eb2e273c2780ed1a68303d6f
Alexey Myalik <myalick2017@yandex.ru>
2021-06-10 22:05:54 +0300

    Add some changes.

---
+++ InterfacesAndAbstractClasses/InterfacesAndAbstractClasses/Base/Coordinate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace InterfacesAndAbstractClasses
+{
+    public struct Coordinate
+    {
+        private double _x;
+        private double _y;
+        private double _z;
+        
+        public Coordinate (double x, double y, double z)
+        {
+            _x = x;
+            _y = y;
+            _z = z;
+        }
+
+        public double Distance(Coordinate coordinate)
+        {
+            return Math.Sqrt(Math.Pow((coordinate._x - _x), 2) + 
+                Math.Pow((coordinate._y - _y), 2) +
+                Math.Pow((coordinate._z - _z), 2));
+        }
+    }
+}
--- InterfacesAndAbstractClasses/InterfacesAndAbstractClasses/FlyingEntryPoint.cs
+++ InterfacesAndAbstractClasses/InterfacesAndAbstractClasses/FlyingEntryPoint.cs
@@ -2,7 +2,7 @@
 
 namespace InterfacesAndAbstractClasses
 {
-    class Program
+    class FlyingEntryPoint
     {
         static void Main(string[] args)
         {
