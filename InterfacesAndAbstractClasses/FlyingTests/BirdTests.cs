60796a04d72a7a26609d17c20f25dd72543bd37a
Alexey Myalik <myalick2017@yandex.ru>
2021-06-10 14:41:52 +0300

    Add BirdTests.

---
+++ InterfacesAndAbstractClasses/FlyingTests/BirdTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FlyingTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
