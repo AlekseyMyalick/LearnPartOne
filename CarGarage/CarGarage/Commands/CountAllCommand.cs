9bdd3bb15b498c832771c5c2a3536a282ee6ed4b
Alexey Myalik <myalick2017@yandex.ru>
2021-06-15 12:56:53 +0300

    Add CountAllCommand class.

--- CarGarage/CarGarage/Commands/CountAllCommand.cs
+++ CarGarage/CarGarage/Commands/CountAllCommand.cs
@@ -1,12 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CarGarage.Interfaces;
+using CarGarage.Basic;
 
 namespace CarGarage.Commands
 {
-    class CountAllCommand
+    public class CountAllCommand : ICommand
     {
+        public void Execute()
+        {
+            Garage.CountAll();
+        }
     }
 }
