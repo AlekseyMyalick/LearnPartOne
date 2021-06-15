92d0b7a753465c01ca221bd31470e123c1ad993c
Alexey Myalik <myalick2017@yandex.ru>
2021-06-15 12:59:12 +0300

    Add CountTypesCommand class.

---
+++ CarGarage/CarGarage/Commands/CountTypesCommand.cs
@@ -0,0 +1,13 @@
+﻿using CarGarage.Interfaces;
+using CarGarage.Basic;
+
+namespace CarGarage.Commands
+{
+    class CountTypesCommand : ICommand
+    {
+        public void Execute()
+        {
+            Garage.CountTypes();
+        }
+    }
+}
