55df39b9e8cce8d5949134e3b0e95bc489dff075
Alexey Myalik <myalick2017@yandex.ru>
2021-06-22 12:35:11 +0300

    Add RemoveAutoExceptionException class.

---
+++ CarPark/CarPark/Exceptions/RemoveAutoException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CarPark.Exceptions
+{
+    [Serializable]
+    public class RemoveAutoExceptionException : Exception
+    {
+        public RemoveAutoExceptionException() { }
+        public RemoveAutoExceptionException(string message) : base(message) { }
+        public RemoveAutoExceptionException(string message, Exception inner) : base(message, inner) { }
+        protected RemoveAutoExceptionException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+}
