31e8eb2305d7f343c251a256aad69ca8937d9021
Alexey Myalik <myalick2017@yandex.ru>
2021-06-23 15:12:46 +0300

    Add CharacterCounter class.

---
+++ DevelopmentAndBuildTools/DevelopmentAndBuildTools/CharacterCounter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevelopmentAndBuildTools
+{
+    public class CharacterCounter
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="characterSet"></param>
+        /// <returns></returns>
+        public static int GetMaximumNumberUnequalConsecutiveCharacters(string characterSet)
+        {
+            if (string.IsNullOrEmpty(characterSet))
+            {
+                return 0;
+            }
+
+            int result = 1;
+            int count = 1;
+
+            for (int i = 0; i < characterSet.Length - 1; i++)
+            {
+                if (characterSet[i].CompareTo(characterSet[i + 1]) != 0)
+                {
+                    count++;
+                }
+                else
+                {
+                    result = GetMaximum(count, result);
+                    count = 1;
+                }
+            }
+
+            return GetMaximum(count, result);
+        }
+
+
+        public static int GetMaximum(int a, int b)
+        {
+            return a > b ? a : b;
+        }
+    }
+}
