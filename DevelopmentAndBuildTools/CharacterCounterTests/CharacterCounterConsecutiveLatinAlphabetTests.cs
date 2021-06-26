a7545ba8b823dd1609b21636f966d289af66a545
Alexey Myalik <myalick2017@yandex.ru>
2021-06-26 14:27:52 +0300

    Add the GetMaximumNumberConsecutiveIdenticalLatinAlphabetLetters_LongString_ReturnsCorrectResults test to the CharacterCounterConsecutiveLatinAlphabetTests class.

--- DevelopmentAndBuildTools/CharacterCounterTests/CharacterCounterConsecutiveLatinAlphabetTests.cs
+++ DevelopmentAndBuildTools/CharacterCounterTests/CharacterCounterConsecutiveLatinAlphabetTests.cs
@@ -48,6 +48,21 @@ namespace DevelopmentAndBuildTools.Tests
             Assert.AreEqual(expected, actual);
         }
 
+        [TestMethod()]
+        [DataRow(",l;fgbl;s;';'pfrpsmmovfldfvv6d6f65h4934)@##%HNKJgknf$#$(V646fGNKh^UWkmlc9ewjjjjdfvkkan24sYEWbmlWYE%^@" +
+            "kjdfnvloikllkk kl;afd e684e84NLFV635456%#^&(lkd;;;sf;ew^^@%#()glnldfvkl;;kkkkkkkkkkkkksdvlasdv666892689265" +
+            "dfvnokdrv;wiefmmkusdc;'d[7%^%# rf;lkllawikmjh4%%#^$@_169dRGJNHJE%#()$)(Yjhjds%&*(bhjsdc557965kfvksdhkmkl##" +
+            "afvkksklfjb&^&*(Thldfvf4564hllsllkln$(#@)U*^(hnjdfvfsvhv64648963dkkksdjkhkbbjhT^&*((T#bkjsdcgjbksugzj    e" +
+            "dfvklndfvf1v64ds67r*435/6y767r974x63rf8697sg216s9g733sdsvbkjkjfhkmse;ocnvhsejfkbvnad58efvhje6$^*%@#@$)kdfd", 13)]
+        public void GetMaximumNumberConsecutiveIdenticalLatinAlphabetLetters_LongString_ReturnsCorrectResults(string value, int expected)
+        {
+            CharacterCounter characterCounter = CreateDefaultCharacterCounter();
+
+            int actual = characterCounter.GetMaximumNumberConsecutiveIdenticalLatinAlphabetLetters(value);
+
+            Assert.AreEqual(expected, actual);
+        }
+
         private CharacterCounter CreateDefaultCharacterCounter()
         {
             return new CharacterCounter();
