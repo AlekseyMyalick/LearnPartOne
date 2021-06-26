fa19f96bd4f6a95126da8e1bf54a9bb4246ca17b
Alexey Myalik <myalick2017@yandex.ru>
2021-06-26 14:20:17 +0300

    Add CharacterCounterConsecutiveLatinAlphabetTests class.

---
+++ DevelopmentAndBuildTools/CharacterCounterTests/CharacterCounterConsecutiveLatinAlphabetTests.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DevelopmentAndBuildTools.Tests
+{
+    [TestClass()]
+    public class CharacterCounterConsecutiveLatinAlphabetTests
+    {
+        [TestMethod()]
+        public void GetMaximumNumberConsecutiveIdenticalLatinAlphabetLetters_Null_ReturnsZero()
+        {
+            CharacterCounter characterCounter = CreateDefaultCharacterCounter();
+
+            int actual = characterCounter.GetMaximumNumberConsecutiveIdenticalLatinAlphabetLetters(null);
+
+            Assert.AreEqual(0, actual);
+        }
+
+        [TestMethod()]
+        public void GetMaximumNumberConsecutiveIdenticalLatinAlphabetLetters_EmptyString_ReturnsZero()
+        {
+            CharacterCounter characterCounter = CreateDefaultCharacterCounter();
+
+            int actual = characterCounter.GetMaximumNumberConsecutiveIdenticalLatinAlphabetLetters(string.Empty);
+
+            Assert.AreEqual(0, actual);
+        }
+
+        [TestMethod()]
+        [DataRow("aaaa[bc5ed/rrrf", 4)]
+        [DataRow("b8ccce@drf9AAAA.", 4)]
+        [DataRow("..bccc]eaaaadr*rf", 4)]
+        public void GetMaximumNumberConsecutiveIdenticalLatinAlphabetLetters_Maximum_ReturnsCorrectResults(string value, int expected)
+        {
+            CharacterCounter characterCounter = CreateDefaultCharacterCounter();
+
+            int actual = characterCounter.GetMaximumNumberConsecutiveIdenticalLatinAlphabetLetters(value);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        private CharacterCounter CreateDefaultCharacterCounter()
+        {
+            return new CharacterCounter();
+        }
+    }
+}
