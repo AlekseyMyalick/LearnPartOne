af36833c7aefb830b90eb0ac572c623933e2ad99
Alexey Myalik <myalick2017@yandex.ru>
2021-06-26 14:19:49 +0300

    Add CharacterCounterConsecutiveIdenticalNumbersTests class.

---
+++ DevelopmentAndBuildTools/CharacterCounterTests/CharacterCounterConsecutiveIdenticalNumbersTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DevelopmentAndBuildTools.Tests
+{
+    [TestClass()]
+    public class CharacterCounterConsecutiveIdenticalNumbersTests
+    {
+        [TestMethod()]
+        public void GetMaximumNumberConsecutiveIdenticalNumbers_Null_ReturnsZero()
+        {
+            CharacterCounter characterCounter = CreateDefaultCharacterCounter();
+
+            int actual = characterCounter.GetMaximumNumberConsecutiveIdenticalNumbers(null);
+
+            Assert.AreEqual(0, actual);
+        }
+
+        [TestMethod()]
+        public void GetMaximumNumberConsecutiveIdenticalNumbers_EmptyString_ReturnsZero()
+        {
+            CharacterCounter characterCounter = CreateDefaultCharacterCounter();
+
+            int actual = characterCounter.GetMaximumNumberConsecutiveIdenticalNumbers(null);
+
+            Assert.AreEqual(0, actual);
+        }
+
+        private CharacterCounter CreateDefaultCharacterCounter()
+        {
+            return new CharacterCounter();
+        }
+    }
+}
