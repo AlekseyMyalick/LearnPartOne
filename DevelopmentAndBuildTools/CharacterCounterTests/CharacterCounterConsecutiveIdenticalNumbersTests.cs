65ebb90203b91810ed99e5edab4ed930e7f8a7d3
Alexey Myalik <myalick2017@yandex.ru>
2021-06-26 14:33:33 +0300

    Add the GetMaximumNumberConsecutiveIdenticalNumbers_Maximum_ReturnsCorrectResults test to the CharacterCounterConsecutiveIdenticalNumbersTests class.

--- DevelopmentAndBuildTools/CharacterCounterTests/CharacterCounterConsecutiveIdenticalNumbersTests.cs
+++ DevelopmentAndBuildTools/CharacterCounterTests/CharacterCounterConsecutiveIdenticalNumbersTests.cs
@@ -35,6 +35,19 @@ namespace DevelopmentAndBuildTools.Tests
             Assert.AreEqual(0, actual);
         }
 
+        [TestMethod()]
+        [DataRow("1111[bc5ed/223f", 4)]
+        [DataRow("b8111e@drf98888.", 4)]
+        [DataRow("..b555]e7777dr*rf", 4)]
+        public void GetMaximumNumberConsecutiveIdenticalNumbers_Maximum_ReturnsZero(string value, int expected)
+        {
+            CharacterCounter characterCounter = CreateDefaultCharacterCounter();
+
+            int actual = characterCounter.GetMaximumNumberConsecutiveIdenticalNumbers(value);
+
+            Assert.AreEqual(expected, actual);
+        }
+
         private CharacterCounter CreateDefaultCharacterCounter()
         {
             return new CharacterCounter();
