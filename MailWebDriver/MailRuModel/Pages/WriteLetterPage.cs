aea8fd19c177619d184c3f62533f7c7d34b07b49
Alexey Myalik <myalick2017@yandex.ru>
2021-07-01 20:28:24 +0300

    Add WriteLetterPage class to MailRuModel project.

---
+++ MailWebDriver/MailRuModel/Pages/WriteLetterPage.cs
@@ -0,0 +1,25 @@
+﻿using OpenQA.Selenium;
+using Waiters;
+
+namespace MailRuModel.Pages
+{
+    /// <summary>
+    /// This is the page describing the page for writing a letter.
+    /// </summary>
+    public class WriteLetterPage : BasePage
+    {
+        private readonly string _driverTitle = "Входящие";
+
+        /// <summary>
+        /// Initializes the fields of the class.
+        /// </summary>
+        /// <param name="driver"></param>
+        public WriteLetterPage(IWebDriver driver) : base(driver)
+        {
+            if (!_driverTitle.Equals(driver.Title))
+            {
+                throw new InvalidElementStateException("This is not the login page");
+            }
+        }
+    }
+}
