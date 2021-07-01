a4d55672f8b2989901d449a6bf1dea145f706c82
Alexey Myalik <myalick2017@yandex.ru>
2021-07-01 20:15:26 +0300

    Add HomePage class to MailRuModel project.

---
+++ MailWebDriver/MailRuModel/Pages/HomePage.cs
@@ -0,0 +1,25 @@
+﻿using OpenQA.Selenium;
+using Waiters;
+
+namespace MailRuModel.Pages
+{
+    /// <summary>
+    /// It is a page describing the home page.
+    /// </summary>
+    public class HomePage : BasePage
+    {
+        private readonly string _driverTitle = "Входящие";
+
+        /// <summary>
+        /// Initializes the fields of the class.
+        /// </summary>
+        /// <param name="driver">Driver.</param>
+        public HomePage(IWebDriver driver) : base(driver)
+        {
+            if (!_driverTitle.Equals(driver.Title))
+            {
+                throw new InvalidElementStateException("This is not the login page");
+            }
+        } 
+    }
+}
