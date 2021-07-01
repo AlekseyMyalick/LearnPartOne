7524e0787a547d08985025c03b8846818c61cb25
Alexey Myalik <myalick2017@yandex.ru>
2021-07-01 16:26:33 +0300

    Add BasePage class to MailRuModel project.

---
+++ MailWebDriver/MailRuModel/Pages/BasePage.cs
@@ -0,0 +1,28 @@
+﻿using OpenQA.Selenium;
+using Waiters;
+
+namespace MailRuModel.Pages
+{
+    /// <summary>
+    /// Represents a class whose methods are common to all web pages.
+    /// </summary>
+    public class BasePage
+    {
+        /// <summary>
+        /// Get or Set driver.
+        /// </summary>
+        public IWebDriver Driver { get; set; }
+
+        /// <summary>
+        /// Initializes the fields of the class 
+        /// and sets the time to wait for events.
+        /// </summary>
+        /// <param name="driver"></param>
+        public BasePage(IWebDriver driver)
+        {
+            Driver = driver;
+            Waiter.Driver = Driver;
+            Waiter.WaitTime = 5000;
+        }
+    }
+}
