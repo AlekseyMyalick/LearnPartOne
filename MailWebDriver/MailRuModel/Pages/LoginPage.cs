0e75ec9c8b1e7f39ae0ff8df049c4585dbcdf1dd
Alexey Myalik <myalick2017@yandex.ru>
2021-07-01 19:41:31 +0300

    Add the TypeUsername method to the LoginPage class, the MailRuModel project.

--- MailWebDriver/MailRuModel/Pages/LoginPage.cs
+++ MailWebDriver/MailRuModel/Pages/LoginPage.cs
@@ -3,7 +3,41 @@ using Waiters;
 
 namespace MailRuModel.Pages
 {
-    public class LoginPage
+    /// <summary>
+    /// Represents a page describing the authorization page.
+    /// </summary>
+    public class LoginPage : BasePage
     {
+        private readonly string _usernameXpath = "//input[@name='username']";
+        private readonly string _enterPasswordButtonXpath = "//span[text()='Ввести пароль']";
+        private readonly string _passwordXpath = "//input[@name='password']";
+        private readonly string _loginButtonXpath = "//span[text()='Войти']";
+        private readonly string _driverTitle = "Авторизация";
+
+        /// <summary>
+        /// Initializes the fields of the class.
+        /// </summary>
+        /// <param name="driver">Driver.</param>
+        public LoginPage(IWebDriver driver) : base(driver)
+        {
+            if (!_driverTitle.Equals(driver.Title))
+            {
+                throw new InvalidElementStateException("This is not the login page");
+            }
+        }
+
+        /// <summary>
+        /// Enters username.
+        /// </summary>
+        /// <param name="username">Username for input.</param>
+        /// <returns>The same page with the entered login.</returns>
+        public LoginPage TypeUsername(string username)
+        {
+            Waiter.WaitElementExists(By.XPath(_usernameXpath));
+
+            Driver.FindElement(By.XPath(_usernameXpath)).SendKeys(username);
+
+            return this;
+        }
     }
 }
