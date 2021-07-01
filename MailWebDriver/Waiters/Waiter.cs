3e65bf672eed57a5a30bddcbde9154754448acce
Alexey Myalik <myalick2017@yandex.ru>
2021-07-01 16:14:04 +0300

    Add the WaitElementExists method to the Waiter class, the Waiters project.

--- MailWebDriver/Waiters/Waiter.cs
+++ MailWebDriver/Waiters/Waiter.cs
@@ -1,12 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
 
 namespace Waiters
 {
-    class Waiter
+    /// <summary>
+    /// Represents a class whose methods implement 
+    /// explicitly waiting for web element events.
+    /// </summary>
+    public static class Waiter
     {
+        /// <summary>
+        /// Get or Set driver.
+        /// </summary>
+        public static IWebDriver Driver { get; set; }
+
+        /// <summary>
+        /// Get or Set waiting time.
+        /// </summary>
+        public static int WaitTime { get; set; }
+
+        /// <summary>
+        /// Waits for the element to appear.
+        /// </summary>
+        /// <param name="webElementLocator">Web element locator.</param>
+        public static void WaitElementExists(By webElementLocator)
+        {
+            new WebDriverWait(Driver, new TimeSpan(0, 0, 0, 0, WaitTime)).
+                Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementExists(webElementLocator));
+        }
     }
 }
--- MailWebDriver/Waiters/WaitersEntryPoint.cs
+++ MailWebDriver/Waiters/WaitersEntryPoint.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Waiters
 {
-    class Program
+    class WaitersEntryPoint
     {
         static void Main(string[] args)
         {
