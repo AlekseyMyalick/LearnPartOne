5950b5efba89e7f5b051ed6022797ede46af17cc
Alexey Myalik <myalick2017@yandex.ru>
2021-07-04 23:55:30 +0300

    Add LoginPage class, GoogleMailModel project.

---
+++ MailWebDriver/GoogleMailModel/Pages/LoginPage.cs
@@ -0,0 +1,25 @@
+﻿using OpenQA.Selenium;
+using Waiters;
+
+namespace GoogleMailModel.Pages
+{
+    /// <summary>
+    /// Represents a page describing the authorization page.
+    /// </summary>
+    public class LoginPage : BasePage
+    {
+        private readonly string _usernameXpath = "//input[@type='email']";
+        private readonly string _driverTitle = "Вход";
+
+        /// <summary>
+        /// Initializes the fields of the class.
+        /// </summary>
+        /// <param name="driver">Driver.</param>
+        public LoginPage(IWebDriver driver) : base(driver)
+        {
+            Waiter.WaitTitleContains(_driverTitle);
+        }
+
+        
+    }
+}
