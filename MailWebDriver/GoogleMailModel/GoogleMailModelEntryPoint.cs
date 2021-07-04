fdf7313f55439ff50aeacb920c9b00337f2a087c
Alexey Myalik <myalick2017@yandex.ru>
2021-07-04 23:46:47 +0300

    Add GoogleMailModel project.

---
+++ MailWebDriver/GoogleMailModel/GoogleMailModelEntryPoint.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoogleMailModel
+{
+    class GoogleMailModelEntryPoint
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
