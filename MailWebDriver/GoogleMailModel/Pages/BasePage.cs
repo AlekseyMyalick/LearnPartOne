55c663b47c1eeff35d40b73acd310fab84f5143b
Alexey Myalik <myalick2017@yandex.ru>
2021-07-04 23:49:03 +0300

    Add BasePage class, GoogleMailModel project.

--- MailWebDriver/GoogleMailModel/Pages/BasePage.cs
+++ MailWebDriver/GoogleMailModel/Pages/BasePage.cs
@@ -1,12 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using OpenQA.Selenium;
+using Waiters;
 
 namespace GoogleMailModel.Pages
 {
-    class BasePage
+    /// <summary>
+    /// Represents a class whose methods are common to all web pages.
+    /// </summary>
+    abstract public class BasePage
     {
+        /// <summary>
+        /// Get or Set driver.
+        /// </summary>
+        public IWebDriver Driver { get; set; }
+
+        /// <summary>
+        /// Initializes the fields of the class 
+        /// and sets the time to wait for events.
+        /// </summary>
+        /// <param name="driver">Driver.</param>
+        public BasePage(IWebDriver driver)
+        {
+            Driver = driver;
+            Waiter.Driver = Driver;
+            Waiter.WaitTime = 10000;
+        }
     }
 }
