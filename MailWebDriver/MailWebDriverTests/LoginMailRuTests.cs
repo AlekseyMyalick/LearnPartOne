27f365c91d436a1b1ba3ae4f8315b6aacdcafaa2
Alexey Myalik <myalick2017@yandex.ru>
2021-07-06 14:15:32 +0300

    Add MailWebDriverTests project.

---
+++ MailWebDriver/MailWebDriverTests/LoginMailRuTests.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+
+namespace MailWebDriverTests
+{
+    public class LoginMailRuTests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
