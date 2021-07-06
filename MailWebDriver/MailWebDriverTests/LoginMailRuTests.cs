66e9aa023ce3e691f5809f4a69152567f6319df7
Alexey Myalik <myalick2017@yandex.ru>
2021-07-06 14:37:18 +0300

    Add OpenLoginPage method to LoginMailRuTests class, MailWebDriverTests project.

--- MailWebDriver/MailWebDriverTests/LoginMailRuTests.cs
+++ MailWebDriver/MailWebDriverTests/LoginMailRuTests.cs
@@ -1,9 +1,27 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 using NUnit.Framework;
 
 namespace MailWebDriverTests
 {
     public class LoginMailRuTests
     {
-       
+        private readonly string _driverPath = @"D:\ChromDriver";
+        private readonly string _loginPagePath = "https://account.mail.ru/login?page=https%3A%2F%2Fe.mail.ru%2Fmessages%2Finbox%3Futm_source%3Dportal%26utm_medium%3Dportal_navigation%26utm_campaign%3De.mail.ru&allow_external=1&from=octavius";
+        private IWebDriver _driver;
+
+        [SetUp]
+        public void OpenLoginPage()
+        {
+            _driver = new ChromeDriver(_driverPath);
+            _driver.Manage().Window.Maximize();
+            _driver.Navigate().GoToUrl(_loginPagePath);
+        }
+
+        [Test]
+        public void SubmitLoginExpectingFailure_EmptyUsername_ReturnNewLoginPage()
+        {
+            Assert.Pass();
+        }
     }
 }
