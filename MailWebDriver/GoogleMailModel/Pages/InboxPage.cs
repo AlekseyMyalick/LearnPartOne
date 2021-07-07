ceb3ff07626ff98b15f82d2cac492e2cd0f878c4
Alexey Myalik <myalick2017@yandex.ru>
2021-07-07 14:21:33 +0300

    Add GetLetters method to InboxPage class, InboxPage project.

--- MailWebDriver/GoogleMailModel/Pages/InboxPage.cs
+++ MailWebDriver/GoogleMailModel/Pages/InboxPage.cs
@@ -1,11 +1,14 @@
-﻿using OpenQA.Selenium;
+﻿using System.Collections.Generic;
+using System.Linq;
+using OpenQA.Selenium;
 using Waiters;
 
 
 namespace GoogleMailModel.Pages
 {
-    class InboxPage : BasePage
+    public class InboxPage : BasePage
     {
+        private readonly string _lettersXpath = "//table[@id=':23']//tbody/child::tr";
         private readonly string _driverTitle = "Входящие";
 
         /// <summary>
@@ -23,6 +26,17 @@ namespace GoogleMailModel.Pages
         public override void PageLoading()
         {
             Waiter.WaitTitleContains(_driverTitle);
+        }
+
+        /// <summary>
+        /// Returns a collection of all incoming emails.
+        /// </summary>
+        /// <returns>The IWebElement collection.</returns>
+        public List<IWebElement> GetLetters()
+        {
+            Waiter.WaitElementIsVisible(By.XPath(_lettersXpath));
+
+            return Driver.FindElements(By.XPath(_lettersXpath)).ToList();
         }
     }
 }
