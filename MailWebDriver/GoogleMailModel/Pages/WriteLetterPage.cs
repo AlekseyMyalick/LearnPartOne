07441fc8fdf8441f8acf5badd85d98773080adaf
Alexey Myalik <myalick2017@yandex.ru>
2021-07-07 12:22:02 +0300

    Add WriteLetterPage class, GoogleMailModel project.

---
+++ MailWebDriver/GoogleMailModel/Pages/WriteLetterPage.cs
@@ -0,0 +1,27 @@
+﻿using OpenQA.Selenium;
+using Waiters;
+
+namespace GoogleMailModel.Pages
+{
+    class WriteLetterPage : BasePage
+    {
+        private readonly string _writeLetterWindowXpath = "";
+
+        /// <summary>
+        /// Initializes the fields of the class.
+        /// </summary>
+        /// <param name="driver"></param>
+        public WriteLetterPage(IWebDriver driver) : base(driver)
+        {
+            PageLoading();
+        }
+
+        /// <summary>
+        /// Waiting for the write letter page to load.
+        /// </summary>
+        public override void PageLoading()
+        {
+            Waiter.WaitElementIsVisible(By.XPath(_writeLetterWindowXpath));
+        }
+    }
+}
