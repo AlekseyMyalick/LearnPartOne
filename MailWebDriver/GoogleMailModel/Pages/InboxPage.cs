9e7e9ef7aa2256bfeed94e1231b2491af1632e11
Alexey Myalik <myalick2017@yandex.ru>
2021-07-07 21:52:52 +0300

    Add IsNotReadedLastIncomingLetter method to InboxPage class, GoogleMailModel project.

--- MailWebDriver/GoogleMailModel/Pages/InboxPage.cs
+++ MailWebDriver/GoogleMailModel/Pages/InboxPage.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using OpenQA.Selenium;
+﻿using OpenQA.Selenium;
 using Waiters;
 
 
@@ -8,8 +6,10 @@ namespace GoogleMailModel.Pages
 {
     public class InboxPage : BasePage
     {
-        private readonly string _lettersXpath = "//div[@class='Cp']/parent::div/child::div[last()]//tbody/child::tr";
         private readonly string _lastIncomingLetter = "//div[@class='Cp']/parent::div/child::div[last()]//tbody/child::tr[1]";
+        private readonly string _lastLetterSender = "//span[@name]";
+        private readonly string _fontWeightCssPropertyName = "font-weight";
+        private readonly int _boldFontWeight = 700;
         private readonly string _driverTitle = "Входящие";
 
         /// <summary>
@@ -27,6 +27,20 @@ namespace GoogleMailModel.Pages
         public override void PageLoading()
         {
             Waiter.WaitTitleContains(_driverTitle);
+        }
+
+        /// <summary>
+        /// Checks if the last received message has been read.
+        /// </summary>
+        /// <returns>True, if the letter is not read, otherwise false.</returns>
+        public bool IsNotReadedLastIncomingLetter()
+        {
+            Waiter.WaitElementIsVisible(By.XPath(_lastIncomingLetter));
+
+            string fontWeight = Driver.FindElement(By.XPath(_lastIncomingLetter + _lastLetterSender))
+                .GetCssValue(_fontWeightCssPropertyName);
+
+            return int.Parse(fontWeight) == _boldFontWeight;
         }
 
         /// <summary>
