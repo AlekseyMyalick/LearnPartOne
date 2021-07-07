9f71e14a46ccda91047f877199710895c585f0be
Alexey Myalik <myalick2017@yandex.ru>
2021-07-07 12:47:27 +0300

    Add InboxPage class, GoogleMailModel project.

---
+++ MailWebDriver/GoogleMailModel/Pages/InboxPage.cs
@@ -0,0 +1,28 @@
+﻿using OpenQA.Selenium;
+using Waiters;
+
+
+namespace GoogleMailModel.Pages
+{
+    class InboxPage : BasePage
+    {
+        private readonly string _driverTitle = "Входящие";
+
+        /// <summary>
+        /// Initializes the fields of the class.
+        /// </summary>
+        /// <param name="driver">Driver.</param>
+        public InboxPage(IWebDriver driver) : base(driver)
+        {
+            PageLoading();
+        }
+
+        /// <summary>
+        /// Waiting for the home page to load.
+        /// </summary>
+        public override void PageLoading()
+        {
+            Waiter.WaitTitleContains(_driverTitle);
+        }
+    }
+}
--- MailWebDriver/GoogleMailModel/Pages/WriteLetterPage.cs
+++ MailWebDriver/GoogleMailModel/Pages/WriteLetterPage.cs
@@ -5,7 +5,9 @@ namespace GoogleMailModel.Pages
 {
     class WriteLetterPage : BasePage
     {
-        private readonly string _writeLetterWindowXpath = "";
+        private readonly string _writeLetterWindowXpath = "//div[@class='AD']";
+        private readonly string _receiverXpath = "//span[text()='Кому']/ancestor::div[@class='fX aiL']//textarea";
+
 
         /// <summary>
         /// Initializes the fields of the class.
@@ -22,6 +24,20 @@ namespace GoogleMailModel.Pages
         public override void PageLoading()
         {
             Waiter.WaitElementIsVisible(By.XPath(_writeLetterWindowXpath));
+        }
+
+        /// <summary>
+        /// Enters receiver.
+        /// </summary>
+        /// <param name="receiver">Receiver.</param>
+        /// <returns>The same page with the entered receiver.</returns>
+        public WriteLetterPage TypeReceiver(string receiver)
+        {
+            Waiter.WaitElementIsVisible(By.XPath(_receiverXpath));
+
+            Driver.FindElement(By.XPath(_receiverXpath)).SendKeys(receiver);
+
+            return this;
         }
     }
 }
