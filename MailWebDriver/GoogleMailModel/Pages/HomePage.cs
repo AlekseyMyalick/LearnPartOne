4939484d2bd1986fff199b4d5cd15de734cac2e4
Alexey Myalik <myalick2017@yandex.ru>
2021-07-07 12:53:48 +0300

    Add OpenInboxPage method to HomePage class, GoogleMailModel project.

--- MailWebDriver/GoogleMailModel/Pages/HomePage.cs
+++ MailWebDriver/GoogleMailModel/Pages/HomePage.cs
@@ -8,7 +8,8 @@ namespace GoogleMailModel.Pages
     /// </summary>
     public class HomePage : BasePage
     {
-        private readonly string _driverTitle = "";
+        private readonly string _inboxButton = "//a[text()='Входящие']/parent::span";
+        private readonly string _driverTitle = "Gmail";
 
         /// <summary>
         /// Initializes the fields of the class.
@@ -16,7 +17,28 @@ namespace GoogleMailModel.Pages
         /// <param name="driver">Driver.</param>
         public HomePage(IWebDriver driver) : base(driver)
         {
+            PageLoading();
+        }
+
+        /// <summary>
+        /// Waiting for the home page to load.
+        /// </summary>
+        public override void PageLoading()
+        {
             Waiter.WaitTitleContains(_driverTitle);
+        }
+
+        /// <summary>
+        /// Opens the inbox page.
+        /// </summary>
+        /// <returns>Inbox page.</returns>
+        public InboxPage OpenInboxPage()
+        {
+            Waiter.WaitElementIsVisible(By.XPath(_inboxButton));
+
+            Driver.FindElement(By.XPath(_inboxButton)).Click();
+
+            return new InboxPage(Driver);
         }
     }
 }
