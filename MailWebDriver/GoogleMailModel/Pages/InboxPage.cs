5aacbb84b4ec4b89784fa1875d4451301b482aab
Alexey Myalik <myalick2017@yandex.ru>
2021-07-07 22:58:25 +0300

    Add EnterReply method to InboxPage class, GoogleMailModel project.

--- MailWebDriver/GoogleMailModel/Pages/InboxPage.cs
+++ MailWebDriver/GoogleMailModel/Pages/InboxPage.cs
@@ -6,12 +6,13 @@ namespace GoogleMailModel.Pages
 {
     public class InboxPage : BasePage
     {
-        private readonly string _lastIncomingLetter = "//div[@class='Cp']/parent::div/child::div[last()]//tbody/child::tr[1]";
-        private readonly string _showHiddenPartButton = "//div[@data-tooltip='Показать скрытую часть']";
-        private readonly string _replyButton = "//span[@role='link'][text()='Ответить']";
-        private readonly string _lastLetterSenderName = "//span[@name]";
-        private readonly string _lastLetterSenderEmail = "//span[@email]";
-        private readonly string _fontWeightCssPropertyName = "font-weight";
+        private readonly string _lastIncomingLetterXpath = "//div[@class='Cp']/parent::div/child::div[last()]//tbody/child::tr[1]";
+        private readonly string _showHiddenPartButtonXpath = "//div[@data-tooltip='Показать скрытую часть']";
+        private readonly string _replyLetterBoxXpath = "//div[@aria-label='Тело письма']/div/br";
+        private readonly string _replyButtonXpath = "//span[@role='link'][text()='Ответить']";
+        private readonly string _lastLetterSenderNameXpath = "//span[@name]";
+        private readonly string _lastLetterSenderEmailXpath = "//span[@email]";
+        private readonly string _fontWeightCssPropertyNameXpath = "font-weight";
         private readonly int _boldFontWeight = 700;
         private readonly string _driverTitle = "Входящие";
 
@@ -38,11 +39,11 @@ namespace GoogleMailModel.Pages
         /// <returns>True, if the letter is not read, otherwise false.</returns>
         public bool IsNotReadedLastIncomingLetter()
         {
-            Waiter.WaitElementIsVisible(By.XPath(_lastIncomingLetter));
+            Waiter.WaitElementIsVisible(By.XPath(_lastIncomingLetterXpath));
 
             string fontWeight = Driver.FindElement(By
-                .XPath(_lastIncomingLetter + _lastLetterSenderName))
-                .GetCssValue(_fontWeightCssPropertyName);
+                .XPath(_lastIncomingLetterXpath + _lastLetterSenderNameXpath))
+                .GetCssValue(_fontWeightCssPropertyNameXpath);
 
             return int.Parse(fontWeight) == _boldFontWeight;
         }
@@ -53,9 +54,9 @@ namespace GoogleMailModel.Pages
         /// <returns>Inbox page.</returns>
         public InboxPage OpenLastIncomingLetter()
         {
-            Waiter.WaitElementIsVisible(By.XPath(_lastIncomingLetter));
+            Waiter.WaitElementIsVisible(By.XPath(_lastIncomingLetterXpath));
 
-            Driver.FindElement(By.XPath(_lastIncomingLetter)).Click();
+            Driver.FindElement(By.XPath(_lastIncomingLetterXpath)).Click();
 
             return this;
         }
@@ -68,10 +69,10 @@ namespace GoogleMailModel.Pages
         /// otherwise it is false.</returns>
         public bool IsExpectedSender(string expectedSender)
         {
-            Waiter.WaitElementIsVisible(By.XPath(_lastIncomingLetter));
+            Waiter.WaitElementIsVisible(By.XPath(_lastIncomingLetterXpath));
 
             string actualSender = Driver.FindElement(By
-                .XPath(_lastIncomingLetter + _lastLetterSenderEmail))
+                .XPath(_lastIncomingLetterXpath + _lastLetterSenderEmailXpath))
                 .Text;
 
             return expectedSender == actualSender;
@@ -83,9 +84,9 @@ namespace GoogleMailModel.Pages
         /// <returns>Inbox page.</returns>
         public InboxPage OpenReplyWindow()
         {
-            Waiter.WaitElementIsVisible(By.XPath(_replyButton));
+            Waiter.WaitElementIsVisible(By.XPath(_replyButtonXpath));
 
-            Driver.FindElement(By.XPath(_replyButton)).Click();
+            Driver.FindElement(By.XPath(_replyButtonXpath)).Click();
 
             return this;
         }
@@ -96,11 +97,26 @@ namespace GoogleMailModel.Pages
         /// <returns>Inbox page.</returns>
         public InboxPage OpenHiddenPartReplyWindow()
         {
-            Waiter.WaitElementIsVisible(By.XPath(_showHiddenPartButton));
+            Waiter.WaitElementIsVisible(By.XPath(_showHiddenPartButtonXpath));
 
-            Driver.FindElement(By.XPath(_showHiddenPartButton)).Click();
+            Driver.FindElement(By.XPath(_showHiddenPartButtonXpath)).Click();
 
             return this;
         }
+
+        /// <summary>
+        /// Enters a reply to the letter.
+        /// </summary>
+        /// <param name="responseText">Text to reply to a letter.</param>
+        /// <returns>Inbox page.</returns>
+        public InboxPage EnterReply(string responseText)
+        {
+            Waiter.WaitElementIsVisible(By.XPath(_replyLetterBoxXpath));
+
+            Driver.FindElement(By.XPath(_replyLetterBoxXpath)).SendKeys(responseText);
+
+            return this;
+        }
+
     }
 }
