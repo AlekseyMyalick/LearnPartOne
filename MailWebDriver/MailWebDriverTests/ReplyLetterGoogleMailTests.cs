be78d5028b25ef5b64fab8c0106ce690f237ba95
Alexey Myalik <myalick2017@yandex.ru>
2021-07-08 16:40:08 +0300

    Add ReplyLetterGoogleMailTests class, MailWebDriverTests project.

---
+++ MailWebDriver/MailWebDriverTests/ReplyLetterGoogleMailTests.cs
@@ -0,0 +1,12 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using NUnit.Framework;
+using GoogleMailModel.Pages;
+
+namespace MailWebDriverTests
+{
+    [TestFixture]
+    public class ReplyLetterGoogleMailTests
+    {
+    }
+}
