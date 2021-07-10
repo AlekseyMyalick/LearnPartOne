ff7f6bf607e69eaee0b9e5267d4932a6854fbff5
Alexey Myalik <myalick2017@yandex.ru>
2021-07-10 21:59:25 +0300

    Add InboxPage class, MailRuModel project.

---
+++ MailWebDriver/MailRuModel/Pages/InboxPage.cs
@@ -0,0 +1,27 @@
+﻿using OpenQA.Selenium;
+using Waiters;
+
+namespace MailRuModel.Pages
+{
+    public class InboxPage : BasePage
+    {
+        private readonly string _driverTitle = "Входящие";
+
+        /// <summary>
+        /// Initializes the fields of the class.
+        /// </summary>
+        /// <param name="driver">Driver.</param>
+        public InboxPage(IWebDriver driver) : base(driver)
+        {
+            PageLoading();
+        }
+
+        /// <summary>
+        /// Waiting for the inbox page to load.
+        /// </summary>
+        public override void PageLoading()
+        {
+            Waiter.WaitTitleContains(_driverTitle);
+        }
+    }
+}
