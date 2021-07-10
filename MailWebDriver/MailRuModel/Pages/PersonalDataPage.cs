5ed8329a1bfbbfe4cb9fd5493d9a6a66911b3168
Alexey Myalik <myalick2017@yandex.ru>
2021-07-10 16:15:40 +0300

    Add PersonalDataPage class, MailRuModel project.

---
+++ MailWebDriver/MailRuModel/Pages/PersonalDataPage.cs
@@ -0,0 +1,27 @@
+﻿using OpenQA.Selenium;
+using Waiters;
+
+namespace MailRuModel.Pages
+{
+    class PersonalDataPage : BasePage
+    {
+        private readonly string _driverTitle = "Личные данные";
+
+        /// <summary>
+        /// Initializes the fields of the class.
+        /// </summary>
+        /// <param name="driver">Driver.</param>
+        public PersonalDataPage(IWebDriver driver) : base(driver)
+        {
+            PageLoading();
+        }
+
+        /// <summary>
+        /// Waiting for the persanal data page to load.
+        /// </summary>
+        public override void PageLoading()
+        {
+            Waiter.WaitTitleContains(_driverTitle);
+        }
+    }
+}
