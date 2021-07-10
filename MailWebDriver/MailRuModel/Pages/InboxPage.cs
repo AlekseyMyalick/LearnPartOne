90196a62f422c11391634ae0791245e645d4c8e6
Alexey Myalik <myalick2017@yandex.ru>
2021-07-10 22:10:34 +0300

    Add OpenLastIncomingLetter method to InboxPage class, MailRuModel project.

--- MailWebDriver/MailRuModel/Pages/InboxPage.cs
+++ MailWebDriver/MailRuModel/Pages/InboxPage.cs
@@ -5,6 +5,7 @@ namespace MailRuModel.Pages
 {
     public class InboxPage : BasePage
     {
+        private readonly string _lastIncomingLetterXpath = "//a[contains(@class, 'letter-list')][1]";
         private readonly string _driverTitle = "Входящие";
 
         /// <summary>
@@ -22,6 +23,19 @@ namespace MailRuModel.Pages
         public override void PageLoading()
         {
             Waiter.WaitTitleContains(_driverTitle);
+        }
+
+        /// <summary>
+        /// Opens the last letter that arrived.
+        /// </summary>
+        /// <returns>Inbox page.</returns>
+        public InboxPage OpenLastIncomingLetter()
+        {
+            Waiter.WaitElementIsVisible(By.XPath(_lastIncomingLetterXpath));
+
+            Driver.FindElement(By.XPath(_lastIncomingLetterXpath)).Click();
+
+            return this;
         }
     }
 }
