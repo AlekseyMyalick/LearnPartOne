d7fb6dddff344814652f66acd5f6efd89256843f
Alexey Myalik <myalick2017@yandex.ru>
2021-07-10 21:51:31 +0300

    Add NicknameChangeTests class, MailWebDriverTests project.

---
+++ MailWebDriver/MailWebDriverTests/NicknameChangeTests.cs
@@ -0,0 +1,11 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using NUnit.Framework;
+
+namespace MailWebDriverTests
+{
+    [TestFixture]
+    public class NicknameChangeTests
+    {
+    }
+}
