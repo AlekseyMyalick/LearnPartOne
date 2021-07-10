3f0ffa8ee7154d4f73c4e452d2566764ee78de79
Alexey Myalik <myalick2017@yandex.ru>
2021-07-10 16:36:07 +0300

    Add SelectPersonalDataSection method to HomePage class, MailRuModel project.

--- MailWebDriver/MailRuModel/Pages/HomePage.cs
+++ MailWebDriver/MailRuModel/Pages/HomePage.cs
@@ -9,6 +9,7 @@ namespace MailRuModel.Pages
     public class HomePage : BasePage
     {
         private readonly string _openSidebarButtonXpath = "//div[contains(@class, 'auth svelte')]//span[contains(@class, 'dropdown')]";
+        private readonly string _personalDataSectionXpath = "//div[text()='Личные данные']/parent::a";
         private readonly string _writeLetterButton = "//span[text()='Написать письмо']";
         private readonly string _driverTitle = "Почта Mail.ru";
 
@@ -53,6 +54,19 @@ namespace MailRuModel.Pages
             Driver.FindElement(By.XPath(_openSidebarButtonXpath)).Click();
 
             return this;
+        }
+
+        /// <summary>
+        /// Selects the personal data section in the sidebar.
+        /// </summary>
+        /// <returns>Personal data page.</returns>
+        public PersonalDataPage SelectPersonalDataSection()
+        {
+            Waiter.WaitElementIsVisible(By.XPath(_personalDataSectionXpath));
+
+            Driver.FindElement(By.XPath(_personalDataSectionXpath)).Click();
+
+            return new PersonalDataPage(Driver);
         }
     }
 }
