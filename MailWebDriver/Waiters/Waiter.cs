8f4643300339b5361df86d97d4098c6920685811
Alexey Myalik <myalick2017@yandex.ru>
2021-07-12 21:59:47 +0300

    Add the WaitElementToBeClickable method to the Waiter class, the Waiters project.

--- MailWebDriver/Waiters/Waiter.cs
+++ MailWebDriver/Waiters/Waiter.cs
@@ -5,7 +5,7 @@ using OpenQA.Selenium.Support.UI;
 namespace Waiters
 {
     /// <summary>
-    /// Represents a class whose methods implement 
+    /// Represents a class whose methods implement
     /// explicitly waiting for web element events.
     /// </summary>
     public static class Waiter
@@ -38,6 +38,16 @@ namespace Waiters
         {
             new WebDriverWait(Driver, new TimeSpan(0, 0, 0, 0, WaitTime)).
                 Until(SeleniumExtras.WaitHelpers.ExpectedConditions.TitleContains(title));
+        }
+
+        /// <summary>
+        /// Waits for an element to become clickable.
+        /// </summary>
+        /// <param name="webElementLocator">Web element locator.</param>
+        public static void WaitElementToBeClickable(By webElementLocator)
+        {
+            new WebDriverWait(Driver, new TimeSpan(0, 0, 0, 0, WaitTime)).
+                Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(webElementLocator));
         }
     }
 }
