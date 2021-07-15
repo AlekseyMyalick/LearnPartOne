d54a4a99a00be51560157e91b69dee6baeb951a3
Alexey Myalik <myalick2017@yandex.ru>
2021-07-15 16:23:09 +0300

    Add User class, Mail project.

---
+++ WebDriverMailAutomation/MailRu/Base/User.cs
@@ -0,0 +1,29 @@
+﻿namespace MailRu.Base
+{
+    /// <summary>
+    /// Represents a class whose fields describe the user.
+    /// </summary>
+    public class User
+    {
+        /// <summary>
+        /// Get or Set email.
+        /// </summary>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Get or Set email.
+        /// </summary>
+        public string Password { get; set; }
+
+        /// <summary>
+        /// Initializes the fields of the class.
+        /// </summary>
+        /// <param name="email">User email.</param>
+        /// <param name="password">User password.</param>
+        public User(string email, string password)
+        {
+            Email = email;
+            Password = password;
+        }
+    }
+}
