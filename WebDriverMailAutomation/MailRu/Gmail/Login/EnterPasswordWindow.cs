4ba8d1821cfd14527d51ca25daeb37d3cbaf4745
Alexey Myalik <myalick2017@yandex.ru>
2021-07-19 01:31:46 +0300

    Add EnterPasswordWindow class, Mail/Gmail/Login project.

---
+++ WebDriverMailAutomation/MailRu/Gmail/Login/EnterPasswordWindow.cs
@@ -0,0 +1,105 @@
+﻿using OpenQA.Selenium;
+using Mail.Base;
+using Mail.Gmail.Home;
+
+namespace Mail.Gmail.Login
+{
+    /// <summary>
+    /// Represents the enter password window of the login page.
+    /// </summary>
+    public class EnterPasswordWindow : BasePage
+    {
+        private readonly string _loginButtonXpath = "//span[text()='Далее']";
+        private readonly string _passwordXpath = "//input[@type='password']";
+        private readonly string _windowCapTitleXpath = "//h1/span[@jsslot]";
+        private readonly string _windowCapTitleText = "Добро пожаловать!";
+
+        /// <summary>
+        /// Initializes the fields of the class.
+        /// </summary>
+        /// <param name="driver">Driver.</param>
+        public EnterPasswordWindow(IWebDriver driver) : base(driver)
+        {
+            WaitPageLoading();
+        }
+
+        /// <summary>
+        /// Waiting for the login page to load.
+        /// </summary>
+        public override void WaitPageLoading()
+        {
+            base.WaitPageLoading();
+
+            new Waiter.Waiter(Driver, WaitTime)
+                .WaitElementIsVisible(By.XPath(_passwordXpath));
+        }
+
+        /// <summary>
+        /// Whether the title is displayed with the desired text on the page.
+        /// </summary>
+        /// <returns>True if the element with the text
+        /// is displayed, otherwise false.</returns>
+        public bool IsWindowCapTitleVisible()
+        {
+            try
+            {
+                new Waiter.Waiter(Driver, WaitTime)
+                .WaitInvisibilityOfElementWithText(By.XPath(_windowCapTitleXpath), _windowCapTitleText);
+
+                return false;
+            }
+            catch (System.Exception)
+            {
+                return true;
+            }
+        }
+
+        /// <summary>
+        /// Enters password.
+        /// </summary>
+        /// <param name="password">Password for input.</param>
+        /// <returns>Enter password window.</returns>
+        public EnterPasswordWindow TypePassword(string password)
+        {
+            new Waiter.Waiter(Driver, WaitTime)
+                .WaitElementIsVisible(By.XPath(_passwordXpath));
+
+            Driver.FindElement(By.XPath(_passwordXpath)).SendKeys(password);
+
+            return this;
+        }
+
+        /// <summary>
+        /// Presses the button sending the password.
+        /// </summary>
+        /// <returns>Returns the enter password window if
+        /// an invalid password was entered, otherwise home page.</returns>
+        public BasePage SubmitPasswordClick()
+        {
+            new Waiter.Waiter(Driver, WaitTime)
+                .WaitElementIsVisible(By.XPath(_loginButtonXpath));
+
+            Driver.FindElement(By.XPath(_loginButtonXpath)).Click();
+
+            if (IsWindowCapTitleVisible())
+            {
+                return new EnterPasswordWindow(Driver);
+            }
+
+            return new HomePage(Driver);
+        }
+
+        /// <summary>
+        /// Fills in the password field and submits it.
+        /// </summary>
+        /// <param name="password">User password.</param>
+        /// <returns>Returns the enter password window if
+        /// an invalid password was entered, otherwise home page.</returns>
+        public BasePage SubmitPassword(string password)
+        {
+            TypePassword(password);
+
+            return SubmitPasswordClick();
+        }
+    }
+}
