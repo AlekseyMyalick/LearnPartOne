1bade8f179435a6c02f7f434f55a211b55b7b97a
Alexey Myalik <myalick2017@yandex.ru>
2021-07-19 01:37:49 +0300

    Add LoginPage class, Mail/MailRu/Login project.

---
+++ WebDriverMailAutomation/MailRu/MailRu/Login/LoginPage.cs
@@ -0,0 +1,69 @@
+﻿using Mail.Base;
+using OpenQA.Selenium;
+
+namespace Mail.MailRu.Login
+{
+    /// <summary>
+    /// Represents a class describing a login page.
+    /// </summary>
+    public class LoginPage : BasePage
+    {
+        private readonly string _errorMessageXpath = "//div[contains(@data-test-id,'error')]/small";
+        private readonly string _loginPagePath = "https://account.mail.ru/login";
+
+        /// <summary>
+        /// Initializes the fields of the class.
+        /// </summary>
+        /// <param name="driver">Driver.</param>
+        public LoginPage(IWebDriver driver) : base(driver) { }
+
+        public void OpenLoginPage()
+        {
+            Driver.Manage().Window.Maximize();
+            Driver.Navigate().GoToUrl(_loginPagePath);
+        }
+
+        /// <summary>
+        /// Returns the error text.
+        /// </summary>
+        /// <returns>Error message text.</returns>
+        public string GetErrorMessage()
+        {
+            new Waiter.Waiter(Driver, WaitTime)
+                .WaitElementIsVisible(By.XPath(_errorMessageXpath));
+
+            string errorMessage = Driver.FindElement(By.XPath(_errorMessageXpath))
+                .Text;
+
+            return errorMessage;
+        }
+
+        /// <summary>
+        /// Performs authorization.
+        /// </summary>
+        /// <param name="user">User.</param>
+        /// <returns>Returns the login window if the email address
+        /// is incorrect, the password entry window if the password
+        /// is incorrect, otherwise home page.</returns>
+        public BasePage LoginAs(User user)
+        {
+            var loginAccountWindow = new LoginAccountWindow(Driver)
+                .SubmitLogin(user.Email);
+
+            if (loginAccountWindow is LoginAccountWindow)
+            {
+                return loginAccountWindow;
+            }
+
+            var enterPasswordWindow = (loginAccountWindow as EnterPasswordWindow)
+                .SubmitPassword(user.Password);
+
+            if (enterPasswordWindow is EnterPasswordWindow)
+            {
+                return enterPasswordWindow;
+            }
+
+            return enterPasswordWindow;
+        }
+    }
+}
