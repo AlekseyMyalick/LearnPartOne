ea56ea2a1e64cf57c2edcb4946a8ff9f2a5cddd0
Alexey Myalik <myalick2017@yandex.ru>
2021-07-19 01:25:19 +0300

    Add Response class, Mail project.

---
+++ WebDriverMailAutomation/MailRu/Base/Response.cs
@@ -0,0 +1,27 @@
+﻿namespace Mail.Base
+{
+    /// <summary>
+    /// Represents a class describing the response.
+    /// </summary>
+    public class Response
+    {
+        /// <summary>
+        /// Get or Set repsponse text.
+        /// </summary>
+        public string Text { get; set; }
+
+        /// <summary>
+        /// Get or set new alias.
+        /// </summary>
+        public string Alias { get; set; }
+
+        /// <summary>
+        /// Initializes the fields of the class.
+        /// </summary>
+        public Response(string text, string alias)
+        {
+            Text = text;
+            Alias = alias;
+        }
+    }
+}
