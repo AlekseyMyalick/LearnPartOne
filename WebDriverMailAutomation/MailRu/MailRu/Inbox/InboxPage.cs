2acef79f88ccce65fde6a7da0f0141d1eb3b5829
Alexey Myalik <myalick2017@yandex.ru>
2021-07-19 01:35:52 +0300

    Add InboxPage class, Mail/MailRu/Inbox project.

---
+++ WebDriverMailAutomation/MailRu/MailRu/Inbox/InboxPage.cs
@@ -0,0 +1,55 @@
+﻿using Mail.Base;
+using OpenQA.Selenium;
+
+namespace Mail.MailRu.Inbox
+{
+    /// <summary>
+    /// It is a page describing the inbox page.
+    /// </summary>
+    public class InboxPage : BasePage
+    {
+        private readonly string _lastIncomingLetterXpath = "//a[contains(@class, 'letter-list')][1]";
+
+        /// <summary>
+        /// Initializes the fields of the class.
+        /// </summary>
+        /// <param name="driver">Driver.</param>
+        public InboxPage(IWebDriver driver) : base(driver)
+        {
+            WaitPageLoading();
+        }
+
+        /// <summary>
+        /// Waiting for the inbox page to load.
+        /// </summary>
+        public override void WaitPageLoading()
+        {
+            base.WaitPageLoading();
+        }
+
+        /// <summary>
+        /// Opens the last letter that arrived.
+        /// </summary>
+        /// <returns>Letter window.</returns>
+        public LetterWindow OpenLastIncomingLetter()
+        {
+            new Waiter.Waiter(Driver, WaitTime)
+                .WaitElementIsVisible(By.XPath(_lastIncomingLetterXpath));
+
+            Driver.FindElement(By.XPath(_lastIncomingLetterXpath)).Click();
+
+            return new LetterWindow(Driver);
+        }
+
+        /// <summary>
+        /// Returns the alias from the last email response.
+        /// </summary>
+        /// <returns>Alias.</returns>
+        public string GetAliasFromReplyLastLetter()
+        {
+            OpenLastIncomingLetter();
+
+            return new LetterWindow(Driver).GetAliasFromReply();
+        }
+    }
+}
