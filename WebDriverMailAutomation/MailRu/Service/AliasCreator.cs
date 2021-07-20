e2d0e2626a9b7e3e88f3f882da4ea39e4e0ef6f6
Alexey Myalik <myalick2017@yandex.ru>
2021-07-21 01:29:02 +0300

    Add AliasCreator class, Mail/Service project.

---
+++ WebDriverMailAutomation/MailRu/Service/AliasCreator.cs
@@ -0,0 +1,11 @@
+﻿using Mail.Base;
+
+namespace MailRu.Service
+{
+    public class AliasCreator
+    {
+        public static Alias OldAlias => new Alias("Robert Langdon");
+
+        public static Alias NewAlias => new Alias("Robdon");
+    }
+}
