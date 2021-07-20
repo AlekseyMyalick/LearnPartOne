e7c353ba714eafd9602b8d05c4dfeb03643be1d8
Alexey Myalik <myalick2017@yandex.ru>
2021-07-21 01:29:38 +0300

    Add ResponseCreator class, Mail/Service project.

---
+++ WebDriverMailAutomation/MailRu/Service/ResponseCreator.cs
@@ -0,0 +1,10 @@
+﻿using Mail.Base;
+using MailRu.Service;
+
+namespace Mail.Service
+{
+    public class ResponseCreator
+    {
+        public static Response SendResponse => new Response("Hello my dear friend!", AliasCreator.NewAlias);
+    }
+}
