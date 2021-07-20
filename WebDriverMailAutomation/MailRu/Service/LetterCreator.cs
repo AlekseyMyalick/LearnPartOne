e5a1cdc5e0200361c110fdd0e1a8db0efa06a5b8
Alexey Myalik <myalick2017@yandex.ru>
2021-07-21 01:29:17 +0300

    Add LetterCreator class, Mail/Service project.

---
+++ WebDriverMailAutomation/MailRu/Service/LetterCreator.cs
@@ -0,0 +1,9 @@
+﻿using Mail.Base;
+
+namespace Mail.Service
+{
+    public class LetterCreator
+    {
+        public static Letter SendLetter => new Letter(UserCreator.UserGmail.Email, "Hello World!");
+    }
+}
