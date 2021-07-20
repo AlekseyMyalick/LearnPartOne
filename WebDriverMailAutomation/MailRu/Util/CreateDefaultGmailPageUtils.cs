330bf4e8c222e968a16c20b040f6274efa505bfb
Alexey Myalik <myalick2017@yandex.ru>
2021-07-21 02:04:21 +0300

    Add CreateDefaultGmailPageUtils class, Mail/Util project.

---
+++ WebDriverMailAutomation/MailRu/Util/CreateDefaultGmailPageUtils.cs
@@ -0,0 +1,28 @@
+﻿using Mail.Gmail.Login;
+using Mail.Gmail.Home;
+using Mail.Gmail.Inbox;
+using OpenQA.Selenium;
+using Mail.Base;
+
+namespace Mail.Util
+{
+    public class CreateDefaultGmailPageUtils
+    {
+        public static LoginPage CreateLoginPage(IWebDriver driver)
+        {
+            LoginPage loginPage = new LoginPage(driver);
+            loginPage.OpenLoginPage();
+
+            return loginPage;
+        }
+        public static HomePage CreateHomePage(IWebDriver driver, User user)
+        {
+            return CreateLoginPage(driver).LoginAs(user) as HomePage;
+        }
+
+        public static InboxPage CreateInboxPage(IWebDriver driver, User user)
+        {
+            return CreateHomePage(driver, user).OpenInboxPage();
+        }
+    }
+}
