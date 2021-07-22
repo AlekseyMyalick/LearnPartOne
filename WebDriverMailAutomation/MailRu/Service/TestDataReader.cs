f1f4d8c0d3c918d49322ae6829b52dc0c0f87a62
Alexey Myalik <myalick2017@yandex.ru>
2021-07-22 14:13:21 +0300

    Add TestDataReader class, MailRu/Service project.

---
+++ WebDriverMailAutomation/MailRu/Service/TestDataReader.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+using System.IO;
+using System.Linq;
+
+namespace MailRu.Service
+{
+    public class TestDataReader
+    {
+        private static string _filePath = "../../../../MailRu/Resources/";
+        private static string _environment = TestContext.Parameters["environment"];
+
+        public static string GetTestData(string key)
+        {
+            return GetValue(_environment, key);
+        }
+
+        private static string GetValue(string environment, string key)
+        {
+            var text = File.ReadAllText($"{_filePath}{environment}.json");
+            JObject json = JObject.Parse(text);
+
+            return json.Descendants()
+                    .OfType<JProperty>()
+                    .Where(p => p.Name == key)
+                    .First()
+                    .Value
+                    .ToString();
+        }
+    }
+}
