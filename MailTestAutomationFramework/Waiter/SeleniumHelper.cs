76737cd3bdc6e98daa3771e8215d5db42b76c986
Alexey Myalik <myalick2017@yandex.ru>
2021-07-25 16:57:56 +0300

    Add SeleniumHelper class, Waiter project.

---
+++ MailTestAutomationFramework/Waiter/SeleniumHelper.cs
@@ -0,0 +1,22 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace Waiter
+{
+    public static class SeleniumHelper
+    {
+        public static By GetByXpath(Type page, string webElementFieldName)
+        {
+            FieldInfo fieldInfo = page.GetField(webElementFieldName);
+
+            FindsByAttribute attribute = (FindsByAttribute)fieldInfo
+                .GetCustomAttributes(typeof(FindsByAttribute), false)
+                .FirstOrDefault();
+
+            return By.XPath(attribute.Using);
+        }
+    }
+}
