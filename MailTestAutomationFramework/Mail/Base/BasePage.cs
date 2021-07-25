4d10b7736e8f578f69e1b7354055b28ee00c4626
Alexey Myalik <myalick2017@yandex.ru>
2021-07-25 16:59:05 +0300

    Add BasePage class, Mail/Base project.

---
+++ MailTestAutomationFramework/Mail/Base/BasePage.cs
@@ -0,0 +1,37 @@
+﻿using OpenQA.Selenium;
+
+namespace Mail.Base
+{
+    /// <summary>
+    /// Represents a class whose methods are common to all web pages.
+    /// </summary>
+    abstract public class BasePage
+    {
+        /// <summary>
+        /// Get the wait time, measured in milliseconds.
+        /// </summary>
+        public int WaitTime => 20000;
+
+        /// <summary>
+        /// Get or Set driver.
+        /// </summary>
+        public IWebDriver Driver { get; set; }
+
+        /// <summary>
+        /// Initializes the fields of the class.
+        /// </summary>
+        /// <param name="driver">Driver.</param>
+        protected BasePage(IWebDriver driver)
+        {
+            Driver = driver;
+        }
+
+        /// <summary>
+        /// Waiting for the page to load.
+        /// </summary>
+        public virtual void WaitPageLoading()
+        {
+            new Waiter.WaiterWrapper(Driver, WaitTime).WaitPageLoading();
+        }
+    }
+}
