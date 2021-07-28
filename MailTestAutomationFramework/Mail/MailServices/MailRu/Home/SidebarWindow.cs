15eecc10a0a6dea7d1cd4caafdd1f5c162a3afbd
Alexey Myalik <myalick2017@yandex.ru>
2021-07-28 16:16:03 +0300

    Add SidebarWindow class, Mail/MailServices/Home project.

---
+++ MailTestAutomationFramework/Mail/MailServices/MailRu/Home/SidebarWindow.cs
@@ -0,0 +1,56 @@
+﻿using OpenQA.Selenium;
+using Mail.Base;
+using Waiter;
+using Mail.Util;
+using Mail.MailServices.MailRu.WriteLetter;
+using Mail.MailServices.MailRu.Inbox;
+
+namespace Mail.MailServices.MailRu.Home
+{
+    /// <summary>
+    /// Represents the sidebar window on the home page.
+    /// </summary>
+    public class SidebarWindow : BasePage
+    {
+        public IWebElement InboxButton => Driver.FindElement(By.XPath("//div[text()='Входящие']"), WaitTime);
+
+        public IWebElement WriteLetterButton => Driver.FindElement(By.XPath("//span[text()='Написать письмо']"), WaitTime);
+
+        /// <summary>
+        /// Initializes the fields of the class.
+        /// </summary>
+        /// <param name="driver">Driver.</param>
+        public SidebarWindow(IWebDriver driver) : base(driver)
+        {
+            base.WaitPageLoading();
+        }
+
+        /// <summary>
+        /// Opens the page for writing a letter.
+        /// </summary>
+        /// <returns>Write letter page.</returns>
+        public WriteLetterPage OpenWriteLetterPage()
+        {
+            new WaiterWrapper(Driver, WaitTime)
+                .WaitElementToBeClickable(WriteLetterButton);
+
+            WriteLetterButton.Click();
+
+            return new WriteLetterPage(Driver);
+        }
+
+        /// <summary>
+        /// Opens the inbox page.
+        /// </summary>
+        /// <returns>Inbox page.</returns>
+        public InboxPage OpenInboxPage()
+        {
+            new WaiterWrapper(Driver, WaitTime)
+                .WaitElementToBeClickable(InboxButton);
+
+            InboxButton.Click();
+
+            return new InboxPage(Driver);
+        }
+    }
+}
