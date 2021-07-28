9a98d585363159d33abd0348c40b729ef66bde84
Alexey Myalik <myalick2017@yandex.ru>
2021-07-28 16:19:48 +0300

    Add SidebarWindow class, Mail/MailServices/Gmail/Home project.

---
+++ MailTestAutomationFramework/Mail/MailServices/Gmail/Home/SidebarWindow.cs
@@ -0,0 +1,39 @@
+﻿using Mail.Base;
+using Mail.MailServices.Gmail.Inbox;
+using Mail.Util;
+using Waiter;
+using OpenQA.Selenium;
+
+namespace Mail.MailServices.Gmail.Home
+{
+    /// <summary>
+    /// Represents the sidebar window on the home page.
+    /// </summary>
+    public class SidebarWindow : BasePage
+    {
+        public IWebElement InboxButton => Driver.FindElement(By.XPath("//a[text()='Входящие']/parent::span"), WaitTime);
+
+        /// <summary>
+        /// Initializes the fields of the class.
+        /// </summary>
+        /// <param name="driver">Driver.</param>
+        public SidebarWindow(IWebDriver driver) : base(driver)
+        {
+            base.WaitPageLoading();
+        }
+
+        /// <summary>
+        /// Opens the inbox page.
+        /// </summary>
+        /// <returns>Inbox page.</returns>
+        public InboxPage OpenInboxPage()
+        {
+            new WaiterWrapper(Driver, WaitTime)
+                .WaitElementToBeClickable(InboxButton);
+
+            InboxButton.Click();
+
+            return new InboxPage(Driver);
+        }
+    }
+}
