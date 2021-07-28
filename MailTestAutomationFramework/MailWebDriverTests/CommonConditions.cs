b526d9fbc22a8117e12758402873bd06ee39fdaf
Alexey Myalik <myalick2017@yandex.ru>
2021-07-28 16:07:46 +0300

    Add CommonConditions class, MailWebDriverTests project.

---
+++ MailTestAutomationFramework/MailWebDriverTests/CommonConditions.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium;
+using NUnit.Framework;
+using Mail.Driver;
+using NUnit.Framework.Interfaces;
+using Mail.Util;
+
+namespace MailWebDriverTests
+{
+    public class CommonConditions
+    {
+        protected IWebDriver _driver;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _driver = DriverSingleton.GetDriver();
+        }
+
+        [TearDown]
+        public void DriverQuit()
+        {
+            if (TestContext.CurrentContext.Result.Outcome == ResultState.Failure)
+            {
+                new ScreenshotMaker().TakeScreenshot(_driver);
+            }
+
+            DriverSingleton.CloseDriver();
+        }
+    }
+}
