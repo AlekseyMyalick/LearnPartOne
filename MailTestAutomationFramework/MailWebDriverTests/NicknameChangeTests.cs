04cb07732bb9d61a675ead4b2a64dc250eb8f293
Alexey Myalik <myalick2017@yandex.ru>
2021-07-28 16:08:21 +0300

    Add NicknameChangeTests class, MailWebDriverTests project.

---
+++ MailTestAutomationFramework/MailWebDriverTests/NicknameChangeTests.cs
@@ -0,0 +1,52 @@
+﻿using NUnit.Framework;
+using MailRuPages = Mail.MailServices.MailRu;
+using GmailPages = Mail.MailServices.Gmail;
+using Mail.Util;
+using Mail.Service;
+
+namespace MailWebDriverTests
+{
+    [TestFixture]
+    public class NicknameChangeTests : CommonConditions
+    {
+        [SetUp]
+        public void SendLetterFromMailRu()
+        {
+            MailRuPages.WriteLetter.WriteLetterPage writeLetter
+                = CreateDefaultMailRuPageUtils.CreateWriteLetterPage(_driver, UserCreator.UserMailRu);
+
+            writeLetter.WriteLetter(LetterCreator.SendLetter);
+        }
+
+        [Test]
+        [Category("All")]
+        public void NicknameChange_CorrectChange_ReturnTrue()
+        {
+            GmailPages.Inbox.InboxPage inboxPage
+                = CreateDefaultGmailPageUtils.CreateInboxPage(_driver, UserCreator.UserGmail);
+            inboxPage.ReplyToLastLetter(ResponseCreator.SendResponse);
+
+            MailRuPages.PersonalData.PersonalDataPage personalDataPageMailRu
+                = CreateDefaultMailRuPageUtils.CreatePersonalDataPage(_driver, UserCreator.UserMailRu);
+
+            personalDataPageMailRu.ChangeNickname(ResponseCreator.SendResponse.AliasName);
+            MailRuPages.Home.HomePage homePageMailRu = personalDataPageMailRu.OpenHomePage();
+            personalDataPageMailRu = homePageMailRu.OpenPersonalDataPage();
+
+            string actualNickname = personalDataPageMailRu.GetNickname();
+
+            Assert.AreEqual(ResponseCreator.SendResponse.AliasName.Name, actualNickname,
+                $"The alias \"{actualNickname}\" does not " +
+                $"match the expected \"{ResponseCreator.SendResponse.AliasName.Name}\".");
+        }
+
+        [TearDown]
+        public void ReplaceWithOldAlias()
+        {
+            MailRuPages.PersonalData.PersonalDataPage personalDataPageMailRu
+                = CreateDefaultMailRuPageUtils.CreatePersonalDataPage(_driver, UserCreator.UserMailRu);
+
+            personalDataPageMailRu.ChangeNickname(AliasCreator.OldAlias);
+        }
+    }
+}
